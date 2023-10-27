95669da58598204ca19836fb25b6ee8da4ca59fe
Chris Stenkamp <cstenkamp@gmail.com>
2023-10-27 14:12:41 +0200

    not searching in the init anymore, can use the same object for multiple searches

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="yagooglesearch",
-    version="1.8.1",
-    author="Brennon Thomas",
+    version="2.0.0",
+    author="Brennon Thomas, Chris Stenkamp",
     author_email="info@opsdisk.com",
     description="A Python library for executing intelligent, realistic-looking, and tunable Google searches.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/opsdisk/yagooglesearch",
+    url="https://github.com/cstenkamp/yagooglesearch",
     packages=setuptools.find_packages(),
     package_data={
         "yagooglesearch": [
@@ -23,6 +23,7 @@ setuptools.setup(
         "beautifulsoup4>=4.9.3",
         "requests>=2.31.0",
         "requests[socks]",
+        "librecaptcha"
     ],
     python_requires=">=3.6",
     license='BSD 3-Clause "New" or "Revised" License',
